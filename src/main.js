4cc0ea600bafbc0a0fb0e7821ee42265a1cb1fec
Miguel Carmona <miguelcarmona996@gmail.com>
2020-05-15 12:50:35 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,54 @@
-import Vue from 'vue'
-import App from './App.vue'
+//Attempt 1
 
-Vue.config.productionTip = false
+// import Vue from 'vue';
+// import { ReactiveBase } from '@appbaseio/reactivesearch-vue';
+// import App from './App';
+
+// Vue.config.productionTip = false;
+// Vue.use(ReactiveBase);
+// console.log('[i] Starting client app')
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+//   components: { App }
+// }).$mount('#app')
+
+//Attempt 2
+
+// import Vue from 'vue'
+// import ReactiveSearch from '@appbaseio/reactivesearch-vue'
+// import App from './App'
+
+// Vue.config.productionTip = false
+
+// Vue.use(ReactiveSearch)
+
+// /* eslint-disable no-new */
+// new Vue({
+//   render: h => h(App),
+//   components: { App }
+// }).$mount('#app')
+
+// import Vue from 'vue';
+// import App from './App.vue';
+// import ReactiveSearch from '@appbaseio/reactivesearch-vue'
+
+// Vue.use(ReactiveSearch);
+// Vue.config.productionTip = false;
+
+// new Vue({
+// 	render: h => h(App),
+// }).$mount('#app');
+
+import Vue from "vue";
+import App from "./App.vue";
+import VueRs from "@appbaseio/reactivesearch-vue";
+
+Vue.use(VueRs);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
